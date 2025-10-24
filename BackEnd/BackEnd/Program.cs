3bb3b0e9a2ddeed2c3d13da18c5755d9d6da0a24
Nikita37373 <kudinovnikita195@gmail.com>
2025-10-24 20:14:43 +0300

    addBackEnd

---
+++ BackEnd/BackEnd/Program.cs
@@ -0,0 +1,53 @@
+using Core.Interfaces;
+using Core.Services;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using WebApiATB;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("MyAtbConnection")));
+
+builder.Services.AddScoped<IImageService, ImageService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+
+// Додали свагера
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(u =>
+    u.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var dir = builder.Configuration["ImagesDir"];
+string path = Path.Combine(Directory.GetCurrentDirectory(), dir);
+Directory.CreateDirectory(path);
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(path),
+    RequestPath = $"/{dir}"
+});
+
+await app.SeedData();
+
+app.Run();
