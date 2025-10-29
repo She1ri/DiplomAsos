358c35c63bc00fcdf07a1db0643b6d7ab6cbb683
novakvova <novakvova@gmail.com>
2025-10-29 20:02:35 +0200

    Get Categories Tree

--- BackEnd/Core/Models/Category/CategoryItemModel.cs
+++ BackEnd/Core/Models/Category/CategoryItemModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Core.Models.Category;
+﻿using Domain.Entities;
+
+namespace Core.Models.Category;
 
 public class CategoryItemModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Image { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string? Image { get; set; } = string.Empty;
+    public IList<CategoryItemModel>? Children { get; set; }
 }
