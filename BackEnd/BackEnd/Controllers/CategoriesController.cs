0fad25c5c67362928d461be39e5260da8420193e
novakvova <novakvova@gmail.com>
2025-11-03 18:22:26 +0200

    Include is good

--- BackEnd/BackEnd/Controllers/CategoriesController.cs
+++ BackEnd/BackEnd/Controllers/CategoriesController.cs
@@ -23,12 +23,10 @@ public class CategoriesController(IMapper mapper,
             .Include(c => c.Children)
                 .ThenInclude(c => c.Children)
                 .ThenInclude(c => c.Children)
+                //.ThenInclude(c => c.Children)
                 .AsNoTracking()
                 .AsQueryable()
-                .OrderByDescending(x=>x.Priority);
-                //.ThenInclude(c => c.Children)
-            //.ProjectTo<CategoryItemModel>(mapper.ConfigurationProvider)
-            //.ToList();
+                .OrderBy(x=>x.Priority);
         var model = mapper.Map<CategoryItemModel[]>(categories)
             
             .ToList();
