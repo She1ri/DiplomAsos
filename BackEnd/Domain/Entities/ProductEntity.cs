0f9e890cf16adabe0b4db3c2d5d562106b7f6c0a
novakvova <novakvova@gmail.com>
2025-11-17 18:35:11 +0200

    Add Product and ProductImages

---
+++ BackEnd/Domain/Entities/ProductEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+[Table("tblProducts")]
+public class ProductEntity : BaseEntity<long>
+{
+    [StringLength(250)]
+    public string Name { get; set; } = String.Empty;
+
+    [StringLength(250)]
+    public string Slug { get; set; } = String.Empty;
+
+    public decimal Price { get; set; }
+
+    [StringLength(10000)]
+    public string? Description { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; }
+
+    public CategoryEntity? Category { get; set; }
+
+
+    public ICollection<ProductImageEntity>? ProductImages { get; set; }
+}
